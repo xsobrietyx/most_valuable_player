cd6b1a21efed1fc79070ed29a63e9791615bf7f9
xsobrietyx <vadym.serdiuk@gmail.com>
2019-07-12 13:48:30 +0300

    Polishing

--- src/test/scala/testutils/UnitSpec.scala
+++ src/test/scala/testutils/UnitSpec.scala
@@ -5,7 +5,7 @@ import java.io.File
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 
 class UnitSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
-  var canonical: String = ""
+  protected var canonical: String = ""
 
   override protected def beforeAll(): Unit = {
     canonical = new File(".").getCanonicalPath + "/src/main/resources/"
