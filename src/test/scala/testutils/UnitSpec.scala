b9786c3e57e938e017794c44c9a8ac7d948c3ba7
xsobrietyx <vadym.serdiuk@gmail.com>
2019-07-12 15:17:38 +0300

    Polishing

--- src/test/scala/testutils/UnitSpec.scala
+++ src/test/scala/testutils/UnitSpec.scala
@@ -2,7 +2,7 @@ package testutils
 
 import java.io.File
 
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+import org.scalatest.{FlatSpec, Matchers}
 
 class UnitSpec extends FlatSpec with Matchers {
   protected lazy val canonical: String = new File(".").getCanonicalPath + "/src/main/resources/"
